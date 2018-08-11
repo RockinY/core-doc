20b338b73f21e43a7a7806f06413aaf4d2d9ec79
Bran Liang <lby89757@hotmail.com>
2018-08-11 12:31:14 +0800

    Add logos

--- doczrc.js
+++ doczrc.js
@@ -5,10 +5,14 @@ export default {
   description: '设计代码规范及元素组件使用说明',
   port: '3009',
   themeConfig: {
+    logo: {
+      src: 'https://yunshemix.oss-cn-hangzhou.aliyuncs.com/logos/logo.png'
+    },
     colors: {
       primary: theme.brand.default,
       link: theme.brand.alt
     }
   },
+  indexHtml: 'src/index.html',
   dest: './build'
 }
